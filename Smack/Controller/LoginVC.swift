318260591c96060181eeaa4629d181244a4f96c2
Fabian Menn <fabi@Fabians-iMac.fritz.box>
2018-09-02 01:19:56 +0200

    Finished LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Fabian Menn on 02.09.18.
+//  Copyright © 2018 Fabian Menn. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Fabian Menn on 02.09.18.
+//  Copyright © 2018 Fabian Menn. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
